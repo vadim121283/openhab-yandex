ece6f59fad7d61bc0f9e3e182acca88c55674778
Vadim <vadim121283@gmail.com>
2019-09-03 21:19:26 +0500

    Delete testclients.js

--- test/testclients.js
+++
@@ -1,7 +0,0 @@
-var clients = require('../db/clients');
-
-clients.findById('5d6bfa4c874bf515c4b8db6c', (error, client) => {
-    if (error) return error;
-    console.log(client);
-    return (null, client);
-});
