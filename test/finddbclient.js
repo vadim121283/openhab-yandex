8a6f1aa1d17ce8ff0af2521c1609dca8e02ba5db
Vadim Akhmatov <vadim121283@gmail.com>
2019-09-03 21:22:38 +0500

    remove tests files

--- test/finddbclient.js
+++
@@ -1,8 +0,0 @@
-let clients = require('../db/clients');
-
-clients.findByYClientId('YandexDialog1', (error, client) => {
-    if (error) return done(error);
-    if (!client) return done(null, false);
-    console.log(client.clientSecret);
-});
-
